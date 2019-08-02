1e41f21c940627f5a5cd717f8606e8f554a5a1b6
Wilfried <wilfried.djopa@gmail.com>
2019-08-02 02:00:21 +0100

    install firebase module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Molux';
+  
+  constructor(){
+    const config = {
+      apiKey: "AIzaSyCKlziXzYBb-qeEGlvcCLv3Hf8ddbiNrZ8",
+      authDomain: "molux-a0633.firebaseapp.com",
+      databaseURL: "https://molux-a0633.firebaseio.com",
+      projectId: "molux-a0633",
+      storageBucket: "",
+      messagingSenderId: "361938234677",
+      appId: "1:361938234677:web:c37e94a9d7b2f12c"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(config);
+  }
 }
